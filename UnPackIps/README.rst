b175f112a668ab6df12f9e902f03d06067ac7134
itdependsnetworks <ken@celenza.org>
2020-07-26 10:09:21 -0400

    update unpack ip, add gitignore

--- UnPackIps/README.rst
+++ UnPackIps/README.rst
@@ -6,73 +6,60 @@ Description
 IpRanges was designed for Network Engineer to quickly unpack IP ranges. The intention is to avoid spreadsheets, and other
 methods of IP importing.
 
-Depedency Modules
+Dependency
 __________
 
-+ ipaddress
-+ re
++ Python3
 
 Usage
 --------------
-+ A pipe "|" seperator is used to seperate subnets within the string.
++ A pipe "|" separator is used to separate subnets within the string, and commas and dashes to expand IP addresses.
 
-Example 1::
+Example 1: Split functionality
 
-          >>> string = "10.1.1.1-5,10,15,50-60|192.168.128.1-5,10,15,100-110"
-          >>> send_ips = class_setup.get_ips(string)
-          >>> print(send_ips)
-          ['10.1.1.1', '10.1.1.2', '10.1.1.3', '10.1.1.4', '10.1.1.5', '192.168.128.1', '192.168.128.2', '192.168.128.3', '192.168.128.4', '192.168.128.5', '10.1.1.10', '10.1.1.15', '192.168.128.10', '192.168.128.15', '10.1.1.50', '10.1.1.51', '10.1.1.52', '10.1.1.53', '10.1.1.54', '10.1.1.55', '10.1.1.56', '10.1.1.57', '10.1.1.58', '10.1.1.59', '10.1.1.60', '192.168.128.100', '192.168.128.101', '192.168.128.102', '192.168.128.103', '192.168.128.104', '192.168.128.105', '192.168.128.106', '192.168.128.107', '192.168.128.108', '192.168.128.109', '192.168.128.110']
+```Python
+>>> from UnPackIps import IpRanges
+>>> class_setup = IpRanges()
+>>> class_setup.get_ips(user_input = "10.1.1.20,25,60-65|192.168.1.1-10")
+['10.1.1.20', '10.1.1.25', '10.1.1.60', '10.1.1.61', '10.1.1.62', '10.1.1.63', '10.1.1.64', '10.1.1.65', '192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.4', '192.168.1.5', '192.168.1.6', '192.168.1.7', '192.168.1.8', '192.168.1.9', '192.168.1.10']
+>>> 
+```
 
-Example 2:
+Example 2: Check for Overlap
 
-		  >>> string = "10.1.1.20,25,60-65|192.168.1.1-10"
-		  >>> call_class = range.IpRanges()
-		  >>> send_string = call_class.get_ips(string)
-	      	  >>> print(send_string)
-		  ['10.1.1.25', '10.1.1.20', '10.1.1.60', '10.1.1.61', '10.1.1.62', '10.1.1.63', '10.1.1.64', '10.1.1.65', '192.168.1.1', '192.168.1.2', '192.168.1.3', 			  '192.168.1.4', '192.168.1.5',
-          	  '192.168.1.6', '192.168.1.7', '192.168.1.8', '192.168.1.9', '192.168.1.10']
+```
+>>> from UnPackIps import IpRanges
+>>> class_setup = IpRanges()
+>>> class_setup.get_ips(user_input = "10.1.1.20-25,21")
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/tmp/Network-Automation/UnPackIps/UnPackIps.py", line 28, in get_ips
+    ipaddress.ip_address(ip)
+ValueError: Overlapping IP: 10.1.1.21
+>>> 
+```
 
-Example 3:
+Example 3: Invalid IP found
 
-		  >>> string = "10.1.1.20,10.1.50.1"
-		  >>> call_class = range.IpRanges()
-		  >>> send_string = call_class.get_ips(string)
-		  >>> print(send_string)
+```
+>>> from UnPackIps import IpRanges
+>>> class_setup = IpRanges()
+>>> class_setup.get_ips(user_input = "10.1.1.20,500")
+Traceback (most recent call last):
+  File "/tmp/Network-Automation/UnPackIps/UnPackIps.py", line 37, in is_ip
+    subnet = '.'.join(octets[0:-1])
+  File "/usr/local/Cellar/python@3.8/3.8.5/Frameworks/Python.framework/Versions/3.8/lib/python3.8/ipaddress.py", line 53, in ip_address
+    raise ValueError('%r does not appear to be an IPv4 or IPv6 address' %
+ValueError: '10.1.1.500' does not appear to be an IPv4 or IPv6 address
 
-		  ['10.1.1.20', '10.1.50.1']
+During handling of the above exception, another exception occurred:
 
-Exceptions
-____________
-
-As the IPs are being assembled they are validated by the ipaddresses module. These exceptions will be passed and the IP(s) wont be added to the list
-A ValueError will be thrown if the subnets on both sides of the pipe "|" are the same. The traceback will display the overlapping subnets
-
-Exception 1:
-
-		  >>> string = "10.1.1.1|10.1.1.2"
-		  >>> call_class = range.IpRanges()
-		  >>> send_string = call_class.get_ips(string)
-		  Traceback (most recent call last):
-		    File "<pyshell#26>", line 1, in <module>
-			  send_string = call_class.get_ips(string)
-		    File "C:\Users\JohnDoe\AppData\Local\Programs\Python\Python37-32\lib\site-packages\IpRanges\AddressRange.py", line 62, in get_ips
-			  IpRanges._check_sub_overlapp(self)
-		    File "C:\Users\JohnDoe\AppData\Local\Programs\Python\Python37-32\lib\site-packages\IpRanges\AddressRange.py", line 36, in _check_sub_overlapp
-			  raise ValueError("Overlapping Subnets between \"|\" %s" % overlap)
-		  ValueError: Overlapping Subnets between "|" 10.1.1. & 10.1.1.
-		  >>>
-
-Exception 2:
-
-		  >>> string = "10.1.1.20,10.1.5a.1"
-		  >>> call_class = range.IpRanges()
-		  >>> send_string = call_class.get_ips(string)
-		 Traceback (most recent call last):
-		    File "<pyshell#35>", line 1, in <module>
-			  send_string = call_class.get_ips(string)
-		    File "C:\Users\JohnDoe\AppData\Local\Programs\Python\Python37-32\lib\site-packages\IpRanges\AddressRange.py", line 47, in get_ips
-			  IpRanges._syntax_check(self, args[0])
-		    File "C:\Users\JohnDoe\AppData\Local\Programs\Python\Python37-32\lib\site-packages\IpRanges\AddressRange.py", line 24, in _syntax_check
-			  raise ValueError("Invalid Syntax")
-		  ValueError: Invalid Syntax
-		  >>>
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/tmp/Network-Automation/UnPackIps/UnPackIps.py", line 26, in get_ips
+    """Check for IP address."""
+  File "/tmp/Network-Automation/UnPackIps/UnPackIps.py", line 39, in is_ip
+    if len(octets) != 4:
+ValueError: NOT a valid IP: 10.1.1.500
+>>> 
+```
--- UnPackIps/UnPackIps.py
+++ UnPackIps/UnPackIps.py
@@ -1,251 +1,57 @@
+"""Helper class to deal with IP ranges."""
+
 import ipaddress
-import re
 
 
 class IpRanges:
-
-    """Intial class which is called from main
-    Methods =  _init_, syntax_check, check_sub_overlapp, get_ips"""
-
-    def __init__(self):
-
-        """Initialize constructors
-        _final_list = final ip list return to use"""
-
-        self._final_list = []
+    """Class for dealing with human readable ip ranges."""
 
     def get_ips(self, user_input: str) -> None:
+        """Take in the user input and split twice using parsing methods for '|', ', and ','."""
+        ip_details = self._get_subnets(user_input)
+        expanded_ip = []
+        check_overlap = {}
+        for item in ip_details:
+            fourth_octets = self._mixrange(item['ranges'])
+            for fourth_octet in fourth_octets:
+                ip = item['subnet'] + '.' + str(fourth_octet)
+                # Verify if there is an IP address
+                self.is_ip(ip)
+                # Check if this IP converted to an integer has been found already, using the dictionary
+                # to determine if this IP was seen already.
+                if check_overlap.get(int(ipaddress.IPv4Address(ip))):
+                    raise ValueError("Overlapping IP: " + ip)
+                check_overlap[int(ipaddress.IPv4Address(ip))] = True
+                expanded_ip.append(ip)
+        return expanded_ip
 
-        """Take in the user input and split twice using \"|\" and \",\""as seperators. Create object from IpParse
-        class to unpack IPs. Use inner funtions to check for overlapping subnets and syntax validation"""
+    def is_ip(self, ip):
+        """Check for IP address."""
+        try:
+            ipaddress.ip_address(ip)
+        except ValueError:
+            raise ValueError("NOT a valid IP: " + ip)
 
-        ip_set = user_input.split("|")
-        subnets = []
-        parsed_ips = []
-        
-        def check_subnet_overlapp() -> None:
+    def _get_subnets(self, user_input):
+        """Private method for getting subnet and fourth octet info."""
+        ip_details = []
+        for subnet in user_input.split('|'):
+            octets = subnet.split('.')
+            subnet = '.'.join(octets[0:-1])
+            ranges = octets[3]
+            if len(octets) != 4:
+                ValueError("There was more than 4 octets found: " + user_input)
+            ip_details.append({"subnet": subnet, "ranges": ranges})
+        return ip_details
 
-            """Checks for overlapping subnets between the pipe \"|\" 10.1.1 | 10.1.1"""
-            ...
-
-            overlap = " & ".join(subnets)
-
-            if len(subnets) != len(set(subnets)) is True:
-                raise ValueError("Overlapping Subnets between \"|\" %s" % overlap)
+    def _mixrange(self, short_hand):
+        """Working with splitting on on command and dashes."""
+        ranges = []
+        # Taken from https://stackoverflow.com/a/18759797
+        for item in short_hand.split(','):
+            if '-' not in item:
+                ranges.append(int(item))
             else:
-                pass
-
-        def syntax_check(subnet_string: str) -> None:
-
-            """Checks to see if the is an [a-zA-Z] char in the string"""
-            ...
-
-            if re.findall(r'[a-zA-Z]', subnet_string):
-                raise ValueError("Invalid Syntax")
-            else:
-                pass
-
-        # Check vaild string input using inner function
-        syntax_check(user_input)
-
-        index = 0
-        for ip_address in ip_set:
-
-            # Find commas in user input to split ip ranges
-            subnet_ranges = ip_set[index].split(",")
-            Class_IpParse = IpParse(ip_address=ip_address, ip_set=ip_set, subnet_ranges=subnet_ranges)
-
-            # Call the class_ipparse ip_list property and add each assembled ip to the instance attribute
-            for i in Class_IpParse.ip_list:
-                parsed_ips.append(i)
-
-            index = index + 1
-            # Store our subnet id to check for overlapping
-            subnets.append(Class_IpParse.network_id[0])
-
-        # Perform housekeeping on our list
-        self._final_list = list(dict.fromkeys(parsed_ips))
-
-        # Check for no overlapping subnets once ip set had been assembled
-        check_subnet_overlapp()
-
-    @property
-    def final_list(self) -> list:
-        return self._final_list
-
-
-def find_range(i: str) -> tuple:
-
-    """Check for start and end range using regex ex. 10-20, 50-60
-        This method is called in the _assemble_ips method. Return for range() interation"""
-    
-    begin_range = None
-    end_range = None
-    
-    if re.findall(r'(?<=\.)[1-9](?=-)', i):
-        begin_range = re.findall(r'(?<=\.)[1-9](?=-)', i)
-    if re.findall(r'(?<=\.)[1-9][0-9](?=-)', i):
-        begin_range = re.findall(r'(?<=\.)[1-9][0-9](?=-)', i)
-    if re.findall(r'(?<=\.)[1-9][0-9][0-9](?=-)', i):
-        begin_range = re.findall(r'(?<=\.)[1-9][0-9][0-9](?=-)', i)
-
-    if re.findall(r'^[1-9](?=-)', i):
-        begin_range = re.findall(r'^[1-9](?=-)', i)
-    if re.findall(r'^[1-9][0-9](?=-)', i):
-        begin_range = re.findall(r'^[1-9][0-9](?=-)', i)
-    if re.findall(r'^[1-9][0-9][0-9](?=-)', i):
-        begin_range = re.findall(r'^[1-9][0-9][0-9](?=-)', i)
-
-    if re.findall(r'(?<=-)[0-9]$', i):
-        end_range = re.findall(r'(?<=-)[0-9]$', i)
-    if re.findall(r'(?<=-)[0-9][0-9]$', i):
-        end_range = re.findall(r'(?<=-)[0-9][0-9]$', i)
-    if re.findall(r'(?<=-)[0-9][0-9][0-9]$', i):
-        end_range = re.findall(r'(?<=-)[0-9][0-9][0-9]$', i)
-
-    return int(begin_range[0]), (end_range[0])
-
-
-class IpParse:
-
-    """All operation for parsing IP string/User Input
-    Methods - _init_, find_range, indivisual_ips, whole_ips, assemble_ips"""
-
-    def __init__(self, ip_address: str = None, ip_set: list = None, subnet_ranges: list = None):
-
-        """Initiate instance attributes"""
-
-        self.ip_address = ip_address
-        self.ip_set = ip_set
-        self.subnet_ranges = subnet_ranges
-        self._ip_list = []
-        self.network_id = re.findall(r'^.*?[0-9]\..*?[0-9]\..*?[0-9]\.', self.ip_address)
-
-        # Initialize class methods used to analyze user input
-        self.indivisual_ips()
-        self.whole_ips()
-        self.whole_subnets()
-        self.assemble_ips()
-
-    def indivisual_ips(self) -> None:
-
-        """Search for indivisual ips. Ex. 1,50,60,100
-        Adds to octet_list and assembled at the bottom of the method"""
-
-        octet_list = []
-
-        for i in self.subnet_ranges:
-
-            if re.findall(r'^[1-9]$', i):
-                octet_1 = re.findall(r'^[1-9]$', i)
-                [octet_list.append(octet) for octet in octet_1]
-
-            elif re.findall(r'^[1-9][0-9]$', i):
-                octet_2 = re.findall(r'^[1-9][0-9]$', i)
-                [octet_list.append(octet) for octet in octet_2]
-
-            elif re.findall(r'^[1-9][0-9][0-9]$', i):
-                octet_3 = re.findall(r'^[1-9][0-9][0-9]$', i)
-                [octet_list.append(octet) for octet in octet_3]
-
-        for ip in octet_list:
-            try:
-                new_ip = self.network_id[0] + ip
-                next_address = ipaddress.ip_address(new_ip)
-                self._ip_list.append(str(next_address))
-            except (IndexError, ValueError):
-                pass
-
-    def whole_subnets(self) -> None:
-
-        """Unpack whole subnets"""
-        
-        ips = None
-        
-        for i in self.subnet_ranges:
-
-            try:
-                if ipaddress.ip_network(i):
-                    ips = list(ipaddress.ip_network(i))
-            except ValueError:
-                continue
-
-            for _ in ips:
-                if re.findall(r'.*[0-9]\..*[0-9]\..*[0-9]\.[0]', str(_)):
-                    continue
-                else:
-                    self._ip_list.append(str(_))
-
-    def whole_ips(self) -> None:
-
-        """Finds whole IPs with in the list, Checks to see if they are valid addresses, stores to list"""
-
-        for i in self.subnet_ranges:
-
-            whole_ip_1 = re.findall(r'^.*[0-9]\..*[0-9]\..*[0-9]\..*[0-9](?=-)', i)
-            for ip in whole_ip_1:
-                try:
-                    ipaddress.ip_address(ip)
-                except ValueError:
-                    pass
-                else:
-                    ipaddress.ip_address(ip)
-                    self._ip_list.append(ip)
-
-            whole_ip_2 = re.findall(r'.*[0-9]\..*[0-9]\..*[0-9]\..*[0-9]', i)
-            for ip in whole_ip_2:
-                try:
-                    ipaddress.ip_address(ip)
-                except ValueError:
-                    pass
-                else:
-                    ipaddress.ip_address(ip)
-                    self._ip_list.append(ip)
-
-    def assemble_ips(self) -> None:
-
-        """ Assembles whole ips with ranges. Ex. 10.1.1.1-10, 192.168.128.50-60. Checks for valid IPs during iteration.
-        User inner function to build IPs"""
-
-        def build_ips(address, start_range, end_range):
-
-            for ip in range(start_range, end_range):
-                next_address = ipaddress.ip_address(address) + 1
-                self._ip_list.append(str(next_address))
-                address = next_address
-
-        for i in self.subnet_ranges:
-
-            if re.findall(r'^.*[0-9]\..*?[0-9]\..*?[0-9]\..*?[0-9](?=-)', i):
-
-                begin_range = re.findall(r'^.*[0-9]\..*?[0-9]\..*?[0-9]\..*?[0-9](?=-)', i)
-                ip_address = begin_range[0]
-                self._ip_list.append(begin_range[0])
-                ip_range = find_range(i)
-                build_ips(ip_address, int(ip_range[0]), int(ip_range[1]))
-
-            elif re.findall(r'^[1-9]-[1-9]$', i):
-
-                ip_range = find_range(i)
-                ip_address = self.network_id[0] + str(ip_range[0])
-                self._ip_list.append(ip_address)
-                build_ips(ip_address, int(ip_range[0]), int(ip_range[1]))
-
-            elif re.findall(r'^[1-9][0-9]-[1-9][0-9]$', i):
-
-                ip_range = find_range(i)
-                ip_address = self.network_id[0] + str(ip_range[0])
-                self._ip_list.append(ip_address)
-                build_ips(ip_address, int(ip_range[0]), int(ip_range[1]))
-
-            elif re.findall(r'^[1-9][0-9][0-9]-[1-9][0-9][0-9]$', i):
-
-                ip_range = find_range(i)
-                ip_address = self.network_id[0] + str(ip_range[0])
-                self._ip_list.append(ip_address)
-                build_ips(ip_address, int(ip_range[0]), int(ip_range[1]))
-
-    @property
-    def ip_list(self) -> list:
-        return self._ip_list
-
+                l,h = map(int, item.split('-'))
+                ranges+= range(l,h+1)
+        return ranges
