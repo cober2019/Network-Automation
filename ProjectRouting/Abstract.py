f8e9cb33df69d3f61b0143ad23482f8582bbb167
cober2019 <53579603+cober2019@users.noreply.github.com>
2020-08-10 19:03:17 -0400

    Add files via upload

--- ProjectRouting/Abstract.py
+++ ProjectRouting/Abstract.py
@@ -2,66 +2,34 @@ from abc import ABC, abstractmethod
 
 
 class Routing(ABC):
-    """Routing table blueprint"""
 
     @abstractmethod
-    def initialize_class_methods(self):
+    def device_login(self):
         pass
 
     @abstractmethod
-    def slash_ten_and_up(self, routing_entry):
+    def device_login(self):
         pass
 
     @abstractmethod
-    def slash_nine_and_below(self, routing_entry):
+    def _find_prefix(self, routing_entry):
         pass
 
     @abstractmethod
-    def no_mask(self, routing_entry):
+    def _get_protocol(self, routing_entry):
         pass
 
     @abstractmethod
-    def protocols_and_metrics(self, routing_entry):
+    def _route_breakdown(self, routing_entry):
         pass
 
     @abstractmethod
-    def routes_unpacked(self):
+    def _write_to_dict(self, route_details):
         pass
 
     @abstractmethod
     def database(self):
         pass
 
-    @property
-    @abstractmethod
-    def host(self):
-        pass
 
-    @abstractmethod
-    def host(self):
-        pass
 
-    @property
-    @abstractmethod
-    def username(self):
-        pass
-
-    @property
-    @abstractmethod
-    def username(self):
-        pass
-
-    @property
-    @abstractmethod
-    def password(self):
-        pass
-
-    @property
-    @abstractmethod
-    def password(self):
-        pass
-
-    @property
-    @abstractmethod
-    def routing(self):
-        pass
