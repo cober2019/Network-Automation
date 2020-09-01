93a3ac2c8282d0a72ec362ba6dbafafd5f16f3e3
cober2019 <53579603+cober2019@users.noreply.github.com>
2020-09-01 01:06:20 -0400

    Add files via upload

--- NETCONF-YANG/BGP/Checks/Prints.py
+++ NETCONF-YANG/BGP/Checks/Prints.py
@@ -14,18 +14,34 @@ def is_instance(list_or_dict) -> list:
 
 def neighbor(config):
 
-    print(f"Neighbor: {config.get('id'):<15}  Remote AS: {config.get('remote-as')} ")
+    print(f"Neighbor: {config.get('id'):<15}  Remote AS: {config.get('remote-as')}")
 
 def address_family(config):
 
     print(f"Neighbor: {config.get('id', {}):<15} Direction: {config.get('route-map', {}).get('inout', 'None'):<7} "
           f"Route-Map: {config.get('route-map', {}).get('route-map-name', 'None'):<15} "
+          f"Networks: {config.get('network', 'None')} "
           f"Soft-Reconfig: {config.get('soft-reconfiguration', 'None')}")
 
 def networks(config):
-    print(f"Networks: {config.get('number', 'None'):<15}  Mask: {config.get('mask', 'None')}")
 
-def redistribution(protocol, key, value):
+    print(f"Networks: {config.get('number', 'None'):<15}  Mask:{config.get('mask', 'None')}")
+
+def legacy_redistribution(protocol, details):
+
+    if protocol == "connected" and isinstance(details, dict):
+        print(f"Protocol: {protocol:<15} "
+              f"{'Route-map: ' + details.get('route-map') + ' -----> '}"
+              f"{'Metric: ' + details.get('metric')}")
+
+    if protocol == "ospf" and isinstance(details, dict):
+        print(f"Protocol: {protocol:<15} Routing ID: {details.get('id') + ' ----->'}  "
+              f"{' Route-map: ' + details.get('non-vrf').get('route-map') + ' ----->'} "
+              f"{'Metric: ' + details.get('non-vrf').get('metric')}")
+
+
+
+def af_redistribution(protocol, key, value):
 
     if protocol == "connected":
         print(f"Protocol: {protocol:<15} Route-map: {value}")
