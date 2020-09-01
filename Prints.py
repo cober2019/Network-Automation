42de6500add44ab856932bb4bb70521a76b19618
cober2019 <53579603+cober2019@users.noreply.github.com>
2020-09-01 01:03:52 -0400

    Add files via upload

---
+++ Prints.py
@@ -0,0 +1,59 @@
+"""Helper functions to search class-map configurations"""
+
+
+def is_instance(list_or_dict) -> list:
+    """Converts dictionary to list"""
+
+    if isinstance(list_or_dict, list):
+        make_list = list_or_dict
+    else:
+        make_list = [list_or_dict]
+
+    return make_list
+
+
+def neighbor(config):
+
+    print(f"Neighbor: {config.get('id'):<15}  Remote AS: {config.get('remote-as')}")
+
+def address_family(config):
+
+    print(f"Neighbor: {config.get('id', {}):<15} Direction: {config.get('route-map', {}).get('inout', 'None'):<7} "
+          f"Route-Map: {config.get('route-map', {}).get('route-map-name', 'None'):<15} "
+          f"Networks: {config.get('network', 'None')} "
+          f"Soft-Reconfig: {config.get('soft-reconfiguration', 'None')}")
+
+def networks(config):
+
+    print(f"Networks: {config.get('number', 'None'):<15}  Mask:{config.get('mask', 'None')}")
+
+def legacy_redistribution(protocol, details):
+
+    if protocol == "connected" and isinstance(details, dict):
+        print(f"Protocol: {protocol:<15} "
+              f"{'Route-map: ' + details.get('route-map') + ' -----> '}"
+              f"{'Metric: ' + details.get('metric')}")
+
+    if protocol == "ospf" and isinstance(details, dict):
+        print(f"Protocol: {protocol:<15} Routing ID: {details.get('id') + ' ----->'}  "
+              f"{' Route-map: ' + details.get('non-vrf').get('route-map') + ' ----->'} "
+              f"{'Metric: ' + details.get('non-vrf').get('metric')}")
+
+
+
+def af_redistribution(protocol, key, value):
+
+    if protocol == "connected":
+        print(f"Protocol: {protocol:<15} Route-map: {value}")
+        print(f"-------------------------------------------------")
+    if protocol == "ospf" and isinstance(value, str):
+        print(f"Protocol: {protocol:<15} Proccess-ID: {value}")
+    elif protocol == "ospf" and isinstance(value, dict):
+        print(f"Protocol: {protocol:<15} Route-map: {value.get('route-map')}")
+
+
+
+
+
+
+
